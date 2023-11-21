301500bdfbee0379c46e5d9b0a599c108a58e045
Md Zahirul Islam Mehadi <108610227+Programmer-Mehadi@users.noreply.github.com>
2023-11-21 10:42:18 +0600

    binary search done.

--- Array/Search/BinarySearchWithRecersive.js
+++ Array/Search/BinarySearchWithRecersive.js
@@ -5,17 +5,16 @@ const numList = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 function findNumber(num, array) {
   let start = 0;
   let end = array.length - 1;
-  while (start <= end) {
-    let mid = start + Math.floor((end - start) / 2);
-    if (array[mid] === num) {
-      return mid;
-    }
-    else if (num < array[mid]) {
-      end = mid - 1;
-    }
-    else {
-      start = mid + 1;
-    }
+  let mid = start + Math.floor((end - start) / 2);
+  // check condition
+  if (array[mid] === num) {
+    return mid;
+  }
+  else if (num < array[mid]) {
+    return findNumber(num, array.slice(0, mid));
+  }
+  else if (num > array[mid]) {
+    return findNumber(num, array.slice(mid + 1, array.length));
   }
   return -1;
 }
